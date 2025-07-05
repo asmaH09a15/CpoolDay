846d3e399046f9ab3a4124bc999b05c916066853
asmaH09a15 <smsn099@yahoo.com>
2025-07-05 12:04:17 +0000

    je commit

---
+++ my_print_ascii.c
@@ -0,0 +1,13 @@
+void my_print_ascii(void){
+    print_char(40);
+    for(int i = 0; i <5; ++i){
+        
+    }
+    
+}
+
+int main(void)
+{
+my_print_ascii();
+return 0;
+}
---
+++ my_print_name.c
@@ -0,0 +1,6 @@
+ void my_print_name(void){
+    print_char( 'a');
+    print_char( 's');
+    print_char('m');
+    print_char('a');
+ }
